ad8eaf2956ed0583bda93a7173e2d2a2ee1abf43
Vivienne Chen <vchen@kellettschool.com>
2026-08-29 11:28:22 +0800

    hi

---
+++ html stuff/main.js
@@ -0,0 +1,6 @@
+document.querySelector("#assignments").style.visibility = 'hidden';
+
+
+document.querySelector("#assignmentsButton").addEventListener('click', () => {
+    document.querySelector("#assignments").style.visibility = 'visible';
+})
