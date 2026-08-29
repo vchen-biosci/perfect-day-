a30dc329a0d5ea1fe9e1c088e943e55842e830ac
Vivienne Chen <vchen@kellettschool.com>
2026-08-29 15:31:53 +0800

    added new stuff

--- html stuff/main.js
+++ html stuff/main.js
@@ -1,6 +1,19 @@
+
 document.querySelector("#assignments").style.visibility = 'hidden';
 
 
 document.querySelector("#assignmentsButton").addEventListener('click', () => {
     document.querySelector("#assignments").style.visibility = 'visible';
+})
+
+
+document.querySelector("#hideAssignmentsButton").addEventListener('click', () => {
+    document.querySelector("#assignments").style.visibility = 'hidden';
+})
+
+
+
+document.querySelector("#changeTasks").addEventListener('click', () => {
+    firstTask = prompt("What's your first task of the day?")
+    document.getElementById("block1").textContent = firstTask;
 })
