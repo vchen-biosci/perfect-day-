8c72b24dbd759243dff6ba144aa53d66bb41c3f8
Vivienne Chen <vchen@kellettschool.com>
2026-08-29 16:00:30 +0800

    hhehre

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+taskNumber = prompt("Hey! How many tasks do you have to complete?")
+taskNumber = parseInt(taskNumber)
+for(var i = 0; i < taskNumber; i++) {
+   document.body.innerHTML += '<div class="block ">block' + i + ''
+}
+
 
 document.querySelector("#assignments").style.visibility = 'hidden';
 
@@ -17,3 +23,5 @@ document.querySelector("#changeTasks").addEventListener('click', () => {
     firstTask = prompt("What's your first task of the day?")
     document.getElementById("block1").textContent = firstTask;
 })
+
+
