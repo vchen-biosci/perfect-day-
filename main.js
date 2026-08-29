93acad0acebf9bfba8e4e95c542d348e55960397
Vivienne Chen <vchen@kellettschool.com>
2026-08-29 17:32:28 +0800

    skhdskfhdf

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-taskNumber = prompt("Hey! How many tasks do you have to complete?")
-taskNumber = parseInt(taskNumber)
+taskNumber = 2 //default!
 
 
 document.querySelector("#assignments").style.visibility = 'hidden';
+
 
 
 document.querySelector("#assignmentsButton").addEventListener('click', () => {
@@ -15,10 +15,26 @@ document.querySelector("#hideAssignmentsButton").addEventListener('click', () =>
 })
 
 
-
-document.querySelector("#changeTasks").addEventListener('click', () => {
+document.querySelector("#changeTask1").addEventListener('click', () => {
     firstTask = prompt("What's your first task of the day?")
     document.getElementById("block1").textContent = firstTask;
 })
 
+document.querySelector("#changeTask2").addEventListener('click', () => {
+    firstTask = prompt("What's your second task of the day?")
+    document.getElementById("block2").textContent = firstTask;
+})
 
+document.querySelector("#addTask").addEventListener('click', () => {
+    taskNumber = taskNumber + 1
+    defaultText = prompt("What's the task about?")
+    const div = document.createElement('div')
+    div.classList.add('block')
+    const h = document.createElement('h2')
+    h.classList = "Block" + taskNumber
+    const p = document.createElement('p')
+    p.classList = defaultText
+    const button = document.createElement('button')
+    button.classList.add('button')
+    button.classList.contains = "Change Task"
+})
